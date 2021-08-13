e4f2b84617d953058573211188097bb4c3529795
unknown <khadkautshav7@gmail.com>
2021-08-14 02:14:54 +0300

    Tests done

---
+++ Articles.Main/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Articles.Main.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Articles.Main.Models.Blog> Blog { get; set; }
+    }
+}
